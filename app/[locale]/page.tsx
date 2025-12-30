2f390b56cd4a018536895410db93436859bafab4
Endika Orube <endikaorve@gmail.com>
2025-12-30 13:06:21 +0100

    style: correcciones barra scroll

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,10 +1,23 @@
 'use client';
 
+import { useEffect } from 'react';
 import { HeroSection, MeSection, ContactSection } from './_components';
 
 export default function Home() {
+  // Aplicar scroll snap al html solo en la home page
+  useEffect(() => {
+    const html = document.documentElement;
+    html.style.scrollSnapType = 'y mandatory';
+    html.style.overflowY = 'scroll';
+
+    return () => {
+      html.style.scrollSnapType = '';
+      html.style.overflowY = '';
+    };
+  }, []);
+
   return (
-    <main className="relative h-svh overflow-y-scroll snap-y snap-mandatory">
+    <main className="relative">
       <HeroSection />
       <MeSection />
       <ContactSection />
