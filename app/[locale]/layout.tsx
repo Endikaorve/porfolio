2be139e61426a69c8fc686e21ddc28ca17330f29
Endika Orube <endikaorve@gmail.com>
2026-01-14 08:44:51 +0100

    chore: correcciones de favicons

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -119,14 +119,7 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 
     // Icons
     icons: {
-      icon: [
-        { url: '/favicon.ico', sizes: 'any' },
-        { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-        { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      ],
-      apple: [
-        { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
-      ],
+      icon: [{ url: '/favicon.ico', sizes: 'any' }],
     },
   };
 }
